c202e8a61825065f48003a2f5348bc36f6f14a1f
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 03:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,8 +75,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i of cart){
-    p(item in cart[0])
-    if(item in cart){
+    if(item in cart['itemName']){
       for(i = 0; i < cart.length; i++) {
         if(cart[i].itemName === item) {
           cart.splice(i, 1);
