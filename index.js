bd355d3046d01500b22a501f9f53825929f27178
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 03:42:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i of cart){
+    p(item in cart)
     if(item in cart){
       for(i = 0; i < cart.length; i++) {
         if(cart[i].itemName === item) {
