b9dbfd0984911224ad55158d400ea8a8b7b50a19
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 03:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,6 @@ function removeFromCart(item) {
       for(i = 0; i < cart.length; i++) {
         if(cart[i].itemName === item) {
           cart.splice(i, 1);
-          p(cart)
         }
       }
     }
