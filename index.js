525c70050ed2dda68729c8935b1e2571f39c5165
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 03:29:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,15 +75,12 @@ function total() {
 
 function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++) {
-   if(cart[i].itemName === 'Blueberries') {
+   if(cart[i].itemName === item) {
      cart.splice(i, 1);
    }
   }
   return cart
 }
-
-
-
 p(removeFromCart("Carrots"))
 
 function placeOrder(cardNumber) {
