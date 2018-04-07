deb3b80bc4d7f11d9f8c45fdfea08367902d2af4
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 04:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,6 @@ function addToCart(item) {
 }
 
 
-addToCart("Carrots")
-addToCart("Blueberries")
-addToCart("Pies")
-
-
 function viewCart() {
   if (cart.length === 0 ){
     return ("Your shopping cart is empty.")
@@ -95,7 +90,6 @@ function placeOrder(cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.")
   }else{
     cart.length = 0
-    
     return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
 }
