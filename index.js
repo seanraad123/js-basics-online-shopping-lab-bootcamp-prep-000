426001de3215bb0835ef9800064f7c22a8ea81e2
Sean Conrad <seanconrad123@gmail.com>
2018-04-07 03:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var cart = [];
 
+ function p(value){
+    console.log(value);
+ }
+
 function getCart() {
  return cart;
 }
@@ -10,20 +14,74 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var randPrice = Math.floor(Math.random() * 100);
+ var items = {itemName: item,itemPrice:randPrice}
+ cart.push(items)
+ return (`${item} has been added to your cart.`)
 }
 
+
+addToCart("Carrots")
+addToCart("Blueberries")
+addToCart("Pies")
+
+
 function viewCart() {
-  // write your code here
+  if (cart.length === 0 ){
+    return ("Your shopping cart is empty.")
+    
+  }else if(cart.length === 1){
+    var str = "In your cart, you have "
+    for (var i of cart){
+       str +=(`${i['itemName']} at $${i['itemPrice']}.`)
+    }
+    return str 
+    
+    //2 or more items
+  }else if(cart.length === 2){
+    str = "In your cart, you have "
+    for (i of cart){
+       str +=(`${i['itemName']} at $${i['itemPrice']}, and `)
+    }
+    str = str.slice(0, -6);
+    str += '.'
+    return str
+  
+  
+  // 3 or more items
+  }else if(cart.length >= 3){
+      str = "In your cart, you have "
+      var last_element = cart[cart.length - 1];
+      for (i of cart){
+        if (i === last_element){
+          str += ('and ')
+         }
+        str +=(`${i['itemName']} at $${i['itemPrice']}, `)
+      }
+      str = str.slice(0, -2);
+      str += '.'
+      return str
+    }
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (var i of cart){
+    sum += parseFloat( i['itemPrice'] );
+  }
+   return (sum)
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  for (i of cart){
+    if (item === i){
+      i.splice()
+    }
+  }
 }
+
+removeFromCart("Carrots")
 
 function placeOrder(cardNumber) {
   // write your code here
